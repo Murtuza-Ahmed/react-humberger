27cca6e0d3404cab2ab1486162b9d973f11448fe
Murtuza-Ahmed <murtuza.programmer@gmail.com>
2024-03-23 00:27:31 +0500

    new component index js and some code file

--- responsive-react-humburger/src/App.jsx
+++ responsive-react-humburger/src/App.jsx
@@ -1,9 +1,17 @@
 import Navbar from "./components/Navbar";
+import { Routes, Route } from "react-router-dom";
+import { About, Home, Services, Contact } from "./components/pages";
 
 const App = () => {
   return (
     <div className="app">
       <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/services" element={<Services />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
     </div>
   );
 };
