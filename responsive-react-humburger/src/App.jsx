2235133dc78446613523e1fc46b3660efc7d6f1e
Murtuza-Ahmed <murtuza.programmer@gmail.com>
2024-03-23 07:30:29 +0500

    paractice

--- responsive-react-humburger/src/App.jsx
+++ responsive-react-humburger/src/App.jsx
@@ -4,15 +4,17 @@ import { About, Home, Services, Contact } from "./components/pages";
 
 const App = () => {
   return (
-    <div className="app">
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/services" element={<Services />} />
-        <Route path="/contact" element={<Contact />} />
-      </Routes>
-    </div>
+    <>
+      <div className="app">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/services" element={<Services />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </div>
+    </>
   );
 };
 export default App;
